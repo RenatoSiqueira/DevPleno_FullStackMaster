284be1cf641f17dc36e108dba6a525d39875dc0b
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-07-09 18:44:10 -0300

    M05 - 1h39min

--- 04. Módulo 04 - Semana 01/Jobify/index.js
+++ 04. Módulo 04 - Semana 01/Jobify/index.js
@@ -1,28 +1,88 @@
 const express = require('express')
 const sqlite = require('sqlite')
+const bodyParser = require('body-parser')
 const app = express()
 
 const dbConnection = sqlite.open('banco.sqlite', { Promise })
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', async (req, res) => {
     const db = await dbConnection
-    const categorias = await db.all('select * from categorias;')
-    console.log(categorias)
+    const categoriasDb = await db.all('select * from categorias;')
+    const vagas = await db.all('select * from vagas;')
+    const categorias = categoriasDb.map(cat => {
+        return {
+            ...cat,
+            vagas: vagas.filter(vaga => vaga.categoria === cat.id)
+        }
+    })
     res.render('home', { categorias })
 })
 
-app.get('/vaga', (req, res) => {
-    res.render('vaga')
+app.get('/vaga/:id', async (req, res) => {
+    //console.log(req.params.id)
+    const db = await dbConnection
+    const vaga = await db.get('select * from vagas where id = ' + req.params.id)
+    //console.log(vaga)
+    res.render('vaga', { vaga })
+})
+
+app.get('/admin', (req, res) => {
+    res.render('admin/home')
+})
+
+app.get('/admin/vagas', async (req, res) => {
+    const db = await dbConnection
+    const vagas = await db.all('select * from vagas;')
+    res.render('admin/vagas', { vagas })
+})
+
+app.get('/admin/vagas/delete/:id', async (req, res) => {
+    const db = await dbConnection
+    await db.run('delete from vagas where id = ' + req.params.id)
+    res.redirect('/admin/vagas')
+})
+
+app.get('/admin/vagas/nova', async (req, res) => {
+    const db = await dbConnection
+    const categorias = await db.all('select * from categorias')
+    res.render('admin/nova-vaga', { categorias })
+})
+
+app.get('/admin/vagas/editar/:id', async (req, res) => {
+    const db = await dbConnection
+    const categorias = await db.all('select * from categorias')
+    const vaga = await db.get('select * from vagas where id = ' + req.params.id)
+    res.render('admin/editar-vaga', { categorias, vaga })
+})
+
+app.post('/admin/vagas/nova', async (req, res) => {
+    const db = await dbConnection
+    const { titulo, descricao, categoria } = req.body
+    await db.run(`insert into vagas (categoria, titulo, descricao) values ('${categoria}', '${titulo}', '${descricao}');`)
+    res.redirect('/admin/vagas')
+})
+
+app.post('/admin/vagas/editar/:id', async (req, res) => {
+    const db = await dbConnection
+    const { titulo, descricao, categoria } = req.body
+    const { id } = req.params
+    await db.run(`update vagas set categoria = '${categoria}', titulo = '${titulo}', descricao = '${descricao}' where id = ${id};`)
+    res.redirect('/admin/vagas')
 })
 
 const init = async () => {
     const db = await dbConnection
     await db.run('create table if not exists categorias(id INTEGER PRIMARY KEY, categoria TEXT);')
-    //const categoria = 'Engineering Team'
+    await db.run('create table if not exists vagas(id INTEGER PRIMARY KEY, categoria INTEGER, titulo TEXT, descricao TEXT);')
+    //const categoria = 'Marketing Team'
     //await db.run(`insert into categorias (categoria) values ('${categoria}');`)
+    //const vaga = 'Fullstack Developer (Remote)'
+    //const descricao = 'Vaga para fullstack developer com conhecimentos ninja'
+    //await db.run(`insert into vagas (categoria, titulo, descricao) values (1, '${vaga}', '${descricao}');`)
 }
 init()
 
