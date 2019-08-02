4965eb5a06572367e343fc30c116c2dba8e8be31
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-08-02 15:49:23 -0300

    M09

--- 09. Módulo 09 - Semana 06/orm-intro/index.js
+++ 09. Módulo 09 - Semana 06/orm-intro/index.js
@@ -5,17 +5,36 @@ const sequelize = new Sequelize(process.env.database, process.env.user, process.
     host: process.env.host
 })
 
+const Pessoa = sequelize.define('Pessoa', {
+    nome: Sequelize.STRING,
+    nascimento: Sequelize.DATE
+})
+
 const Usuario = sequelize.define('Usuario', {
     usuario: Sequelize.STRING,
     senha: Sequelize.STRING
 })
 
+const Projeto = sequelize.define('Projeto', {
+    nome: sequelize.STRING
+})
+
+Pessoa.hasOne(Usuario)
+Usuario.belongsTo(Pessoa)
+Pessoa.hasMany(Projeto)
+Projeto.belongsTo(Pessoa)
+
 const testDb = async () => {
-    await sequelize.sync()
-    Usuario.create({
+    await sequelize.sync({ force: true })
+    const pessoa = await Pessoa.create({
+        nome: 'Renato',
+        nascimento: '1981-01-01'
+    })
+    const user = Usuario.create({
         usuario: 'renato',
         senha: '12345'
     })
+    user.setPessoa(pessoa)
     //sequelize.authenticate().then(() => console.log('Connected'))
 }
 
