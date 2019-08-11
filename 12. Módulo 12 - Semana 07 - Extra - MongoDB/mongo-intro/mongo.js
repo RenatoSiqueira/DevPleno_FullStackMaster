9dfe1276c05b6d5a2037c0680ad1aedf26066008
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-08-10 22:56:49 -0300

    M12

---
+++ 12. Módulo 12 - Semana 07 - Extra - MongoDB/mongo-intro/mongo.js
@@ -0,0 +1,12 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/intro-mongo', (err, db) => {
+    const pessoas = db.collection('pessoas')
+    pessoas.insert({
+        nome: 'Renato',
+        nascimento: '1981-06-15'
+    }, (err, res) => {
+        console.log(err, res)
+    })
+})
