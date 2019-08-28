f6f5a2e1321bace6e776298bed111db6d0c9f030
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-08-28 00:48:41 -0300

    Gzip

---
+++ 22. Módulo 22 - Semana 12 - Otimização/gzip-teste/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const compression = require('compression')
+
+
+
+const largeObject = []
+for (let i = 0; i < 70000; i++) {
+    largeObject.push({
+        name: 'Renato',
+        address: 'address',
+        anotherField: 'aaaaaa'
+    })
+}
+
+app.use(compression({ level: 9 }))
+app.get('/', (req, res) => {
+    res.send(largeObject)
+})
+
+app.listen(3000, () => console.log('Running...'))
