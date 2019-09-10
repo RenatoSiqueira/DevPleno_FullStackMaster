ea979f874b816020c073f673e9094e2402e904b8
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-09-10 15:47:09 -0300

    Prepare to unit Tests

--- 26. Módulo 26 - Módulo 05/tabuada/controllers/tabuada.js
+++ 26. Módulo 26 - Módulo 05/tabuada/controllers/tabuada.js
@@ -1,11 +1,24 @@
 const list = (req, res) => {
-    res.render('tabuada/list')
+    const numeros = []
+    for (let i = 0; i <= 100; i++) {
+        numeros.push(i)
+    }
+    res.render('tabuada/list', { numeros })
 }
 
 const tabuada = (req, res) => {
-
+    const tabuadas = []
+    const num = req.params.num
+    for (let i = 0; i <= 100; i++) {
+        tabuadas.push({
+            num,
+            i,
+            resultado: num * i
+        })
+    }
     res.render('tabuada/tabuada', {
-        num: req.params.num
+        num: req.params.num,
+        tabuadas
     })
 }
 
